4e5f7ef4f3b19fa5e3ff2b8d89f454f1f8b20b43
Nguyễn Minh Tiến <nguyentien7a8@gmail.com>
2025-05-09 20:24:21 +0700

    chore: cors fix

--- src/main/java/vn/ptithcm/shopapp/config/OpenAPIConfig.java
+++ src/main/java/vn/ptithcm/shopapp/config/OpenAPIConfig.java
@@ -60,7 +60,7 @@ public class OpenAPIConfig {
                 .info(createApiInfo())
                 .servers(List.of(
                         createServer("http://localhost:8080", "Server URL in Development environment"),
-                        createServer("https://bromel.vn", "Server URL in Production environment")))
+                        createServer("https://shopapp-backend-9po5.onrender.com/", "Server URL in Production environment")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
