b8c4e8745b2b6cc8c6dd15dc6874f32dc7186b84
Nguyễn Minh Tiến <nguyentien7a8@gmail.com>
2025-05-09 16:58:28 +0700

    chore: cors fix

--- src/main/java/vn/ptithcm/shopapp/config/CorsConfig.java
+++ src/main/java/vn/ptithcm/shopapp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4173","http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4173","http://localhost:5173","https://shopapp-backend-9po5.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
                 "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type",
