dee6da12e9f9150e161944914a99f022f7c93cd4
Nguyễn Minh Tiến <nguyentien7a8@gmail.com>
2025-05-12 21:30:03 +0700

    chore: swagger config

--- src/main/java/vn/ptithcm/shopapp/config/OpenAPIConfig.java
+++ src/main/java/vn/ptithcm/shopapp/config/OpenAPIConfig.java
@@ -59,8 +59,9 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(createApiInfo())
                 .servers(List.of(
-                        createServer("http://localhost:8080", "Server URL in Development environment"),
-                        createServer("https://shopapp-backend-9po5.onrender.com/", "Server URL in Production environment")))
+
+                        createServer("https://shopapp-backend-9po5.onrender.com/", "Server URL in Production environment"),
+                        createServer("http://localhost:8080", "Server URL in Development environment")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
